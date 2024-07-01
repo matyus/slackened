41f745a73fb6cc31a5b69463d3422d8a8234577c
Michael Matyus <michael@maty.us>
2024-07-01 00:12:42 -0400

    fix namespace

--- examples/message_validate_example.rb
+++ examples/message_validate_example.rb
@@ -16,13 +16,10 @@ payload = JSON.load_file('./examples/payload.json')
 
 timestamp = '1719262409'
 
-data = "v0:#{timestamp}:#{payload}"
-
-puts "PAYLOAD: #{payload}"
-puts "DATA:    #{data}"
-
-puts Slackened::Request.new(
+result = Slackened::Authentication::Request.new(
   timestamp:,
   signature: 'v0=ec64d01b232bc392a07464d98bb904ac83a40995d9bdf5de03c475ecba29dc6c',
   body: payload
 ).valid?
+
+puts "RESULT: #{result}"
